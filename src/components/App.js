94b98a1b347f181e7688674ab07b7676bfd3c665
Kenny Okada <kokada32@gmail.com>
2021-09-08 15:37:39 -0700

    communicated VideoItem with App to click on a video and recieve video object

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,8 @@ import VideoList from './VideoList';
 
 class App extends React.Component {
     state = {
-        videos: []
+        videos: [],
+        selectedVideo: null
     };
 
     onTermSubmit = async term => {
@@ -18,11 +19,15 @@ class App extends React.Component {
         this.setState({ videos: response.data.items });
     };
 
+    onVideoSelect = video => {
+        console.log('from the app!', video);
+    };
+
     render() {
         return (
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onTermSubmit} />
-                <VideoList videos={this.state.videos} />
+                <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
             </div>
         );
     };

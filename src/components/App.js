c1e05be1ecf95b5771aad6e91abd0ae7d3a66c5e
Kenny Okada <kokada32@gmail.com>
2022-02-28 15:38:56 -0800

    misspelling change

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ const App = () => {
     const [selectedVideo, setSelectedVideo] = useState(null);
 
     useEffect(() => {
-        onTermSubmit('buildings');
+        onTermSubmit('cars');
     }, []);
 
     const onTermSubmit = async term => {
@@ -23,10 +23,6 @@ const App = () => {
         setSelectedVideo(response.data.items[0]);
     };
 
-    const onVideoSelect = video => {
-        setSelectedVideo(video);
-    };
-
     return (
         <div className="ui container">
             <SearchBar onFormSubmit={onTermSubmit} />
@@ -36,7 +32,7 @@ const App = () => {
                         <VideoDetail video={selectedVideo} />
                     </div>
                     <div className="five wide column">
-                        <VideoList onVideoSelect={onVideoSelect} videos={videos} />
+                        <VideoList onVideoSelect={setSelectedVideo} videos={videos} />
                     </div>
                 </div>
             </div>
